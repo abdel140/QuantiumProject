7c0c2c4a0c91d66be7dcac5e4683c9b6cf3beba6
Lilian Berna <arphax33@gmail.com>
2021-11-23 11:17:18 +0100

    init files

---
+++ main.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html lang="en">
+<head>
+    <meta charset="utf-8">
+    <title>Post-Quantum checker</title>
+    <base href="/">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+    <link rel="icon" type="image/x-icon" href="favicon.ico">
+    <link rel="stylesheet" type="text" href="main.css">
+</head>
+<body>
+<nav>
+
+</nav>
+
+<div class="main-wrapper">
+    <?php
+    echo('test')
+    ?>
+</div>
+
+<footer>
+
+</footer>
+</body>
