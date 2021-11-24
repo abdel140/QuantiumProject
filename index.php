7228b73b78a8fed152c53562aa5dcc47d37fa1fc
abdelilah <abdelilah.elouardi@epsi.fr>
2021-11-24 16:21:30 +0100

    integration template non fini

--- index.php
+++ index.php
@@ -14,64 +14,82 @@
         }
     }
 ?>
-
-
-<!doctype html>
+<!DOCTYPE html>
 <html lang="en">
 <head>
     <meta charset="utf-8">
     <title>Post-Quantum checker</title>
     <base href="/">
     <meta name="viewport" content="width=device-width, initial-scale=1">
-    <link rel="icon" type="image/x-icon" href="favicon.ico">
-    <link rel="stylesheet" type="text" href="main.css">
+    <link rel="stylesheet" type="text/css" href="/QuantiumProject/main.css">
+    <!-- Bootstrap tags -->
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.min.js" integrity="sha384-QJHtvGhmr9XOIpI6YVutG+2QOK9T+ZnN4kzFN1RtK3zEFEIsxhlmWl5/YESvpZ13" crossorigin="anonymous"></script>
+    <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/js/bootstrap.bundle.min.js" integrity="sha384-ka7Sk0Gln4gmtz2MlQnikT1wXgYsOg+OMhuP+IlRH9sENBO0LRn5q+8nbTov4+1p" crossorigin="anonymous"></script>
+    <!-- FontAwsome -->
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0-beta3/css/all.min.css" integrity="sha512-Fo3rlrZj/k7ujTnHg4CGR2D7kSs0v4LLanw2qksYuRlEzO+tcaEPQogQ0KaoGN26/zrn20ImR1DfuLWnOo7aBA==" crossorigin="anonymous" referrerpolicy="no-referrer" />
 </head>
 <body>
-<nav>
-    <h1>Post-Quantum Checker</h1>
-</nav>
+<header>
+    <nav>
+        <div class="nav-top">
+            <h1>POST-QUANTIUM CHECKER</h1>
+        </div>
+    </nav>
+</header>
 
-<div class="main-wrapper">
-    <span class="divider horizontal"></span>
-
-    <form class="card" action="index.php?action=test_certificate" method="post" enctype="multipart/form-data">
-        <input id="file-input" type="file" name="certificate" onchange="this.form.submit();">
-        <label for="file-input">Upload HTTPS Certificate</label>
-    </form>
-
-<?php
-    if (!empty($is_quantum_proof)) {
-        if ($is_quantum_proof) {
-?>
-            <div class="card">
-                <h1>YOU ARE QUANTUM PROOF</h1>
-                <img id="gif" src="assets/bravo.gif" alt="People applause">
+<div class="container">
+    <div class="main-wrapper">
+        <div class="top-text-description">
+            <p>Check if your HTTPS certficate is post-quantum proof or not.</p>
+        </div>
+        <hr class="separator-header">
+        <form action="index.php?action=test_certificate" method="post" enctype="multipart/form-data">
+            <div class="upload-area">
+                <input id="file-input" type="file" name="certificate" onchange="this.form.submit();" >
+                <i class="fas fa-upload"></i>
+                <label for="file-input">Upload HTTPS Certificate</label>
             </div>
-<?php
-        } else {
-?>
-            <div class="card">
-                <h1>HOW TO BE QUANTUM PROOF ?</h1>
-                <p></p>
-            </div>
-<?php
+        </form>
+        <?php
+        if (!empty($is_quantum_proof)) {
+            if ($is_quantum_proof) {
+                ?>
+                <div class="card">
+                    <h1>YOU ARE QUANTUM PROOF</h1>
+                    <img id="gif" src="assets/bravo.gif" alt="People applause">
+                </div>
+                <?php
+            } else {
+                ?>
+                <div class="card">
+                    <h1>HOW TO BE QUANTUM PROOF ?</h1>
+                    <p></p>
+                </div>
+                <?php
+            }
         }
-    }
-?>
+        ?>
+    </div>
 </div>
 
-<footer>
-    <h1>Do you want more informations ?</h1>
-
-    <div class="footer-infos">
-
+<footer class="row">
+    <div class="col-8 information-footer">
+        <h5 class="">Do you want more </br> informations ?</h5>
     </div>
-
-    <span class="divider vertical"></span>
-
-    <div class="footer-img">
-        <img src="assets/epsi.png" alt="Logo of EPSI school">
-        <img src="assets/hep.png" alt="Logo of HEP group">
+    <div class="footer-logos col-4">
+        <div class="row">
+            <div class="col-2">
+                <div class="separator-footer"></div>
+            </div>
+            <div class="col-5" align="center">
+                <img src="QuantiumProject/assets/epsi.png" alt="Logo of EPSI school">
+            </div>
+            <div class="col-5 mx-auto" align="center">
+                <img src="QuantiumProject/assets/hep.png" alt="Logo of HEP group">
+            </div>
+        </div>
     </div>
 </footer>
 </body>
+</html>
